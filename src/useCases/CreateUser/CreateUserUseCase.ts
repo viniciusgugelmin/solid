97b2505a5e948089953b290ea4674a789e6954d0
Vinicius <vinigugelmin@hotmail.com>
2022-06-28 14:18:00 -0300

    chore: SOLID implemented at CreateUserUseCase with mailProvider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,14 +1,20 @@
-import {User} from "../../entities/User";
-import {IUsersRepository} from "../../repositories/IUsersRepository";
-import {ICreateUserRequestDTO} from "./CreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { IMailProvider } from "../../providers/IMailProvider";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {
-  }
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute(data: ICreateUserRequestDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(
-        data.email
+      data.email
     );
 
     if (userAlreadyExists) {
@@ -18,5 +24,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: process.env.MAIL_NAME,
+        email: process.env.MAIL_ADDRESS,
+      },
+      subject: "Welcome!",
+      body: `<p>Hello <strong>${data.name}</strong>!</p>`,
+    });
   }
 }
