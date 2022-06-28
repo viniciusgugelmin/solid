5db939feca875357bb04d01e3a851b0d23aa97a8
Vinicius <vinigugelmin@hotmail.com>
2022-06-28 14:43:05 -0300

    chore: SOLID implemented at CreateUserUseCase with route added

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { createUserController } from "./useCases/CreateUser";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send("Hello World!");
+router.post("/", (req, res) => {
+  return createUserController.handle(req, res);
 });
 
 export { router };
