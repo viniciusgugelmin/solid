1dcdefee7ad9be3fbdd05202a4b9fd326b1c9009
Vinicius <vinigugelmin@hotmail.com>
2022-06-28 14:30:43 -0300

    chore: SOLID implemented at CreateUserUseCase with MailtrapMailProvider

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,32 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from "nodemailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "1925ddf5cf84a7",
+        pass: "9024cd40218724",
+      },
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
