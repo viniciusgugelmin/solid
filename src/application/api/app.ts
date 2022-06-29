1e3852b1a7fec8c731e3611986c943b4c97734ba
Vinicius <vinigugelmin@hotmail.com>
2022-06-29 13:50:39 -0300

    feat: updated users migration

--- src/application/api/app.ts
+++ src/application/api/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { index } from "./routes";
+import { routes } from "./routes/routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(index);
+app.use(routes);
 
 export { app };
--- src/application/api/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { userRouter } from "./userRputer";
-
-const index = Router();
-
-index.use("/users", userRouter);
-
-export { index };
