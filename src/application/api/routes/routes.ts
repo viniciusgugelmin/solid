27cc6648ae0aa0c73c079a2254aaf5480e8c7032
Vinicius <vinigugelmin@hotmail.com>
2022-08-23 16:50:40 -0300

    feat: base route added

--- src/application/api/routes/routes.ts
+++ src/application/api/routes/routes.ts
@@ -3,6 +3,10 @@ import { userRouter } from "./user.routes";
 
 const routes = Router();
 
+routes.get("/", (req, res) => {
+  res.send("OK");
+});
+
 routes.use("/users", userRouter);
 
 export { routes };
