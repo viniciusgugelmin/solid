f7b7ae4aaf34cffa76c3d3a447917a74f9eb20ae
Vinicius <vinigugelmin@hotmail.com>
2022-09-12 13:41:47 -0300

    test: tests updated to base route

--- src/useCases/CreateUser/__tests__/integration/CreateUserController.spec.ts
+++ src/useCases/CreateUser/__tests__/integration/CreateUserController.spec.ts
@@ -6,53 +6,59 @@ import { MailProvider } from "../../../../providers/Mail/MailProvider";
 import { usersRepository } from "../../../../repositories";
 import { usersHelper } from "../../../../helpers";
 
-describe("CreateUserController", () => {
-  const server = new Server();
-  const app = server.app;
+const server = new Server();
+const app = server.app;
 
-  beforeEach(async () => {
-    await database.truncateTable("users");
-    jest.clearAllMocks();
-  });
-  afterAll(async () => {
-    await database.truncateTable("users");
-    await database.disconnect();
-  });
+beforeEach(async () => {
+  await database.truncateTable("users");
+  jest.clearAllMocks();
+});
 
-  it("should create an user", async () => {
-    jest
-      .spyOn(MailProvider.prototype, "sendMail")
-      .mockImplementation(async () => {});
+afterAll(async () => {
+  await database.truncateTable("users");
+  await database.disconnect();
+});
 
-    const user = usersFactory.generate();
-    const { status, body } = await supertest(app).post("/api/users").send(user);
+describe("POST /users", () => {
+  const request = async (body: any) =>
+    await supertest(app).post("/users").send(body);
 
-    expect(status).toBe(201);
-    expect(body).toStrictEqual(
-      expect.objectContaining({
-        message: "User created",
-        status: "OK",
-        statusCode: 201,
-        data: {},
-      })
-    );
+  describe("success", () => {
+    it("should create an user", async () => {
+      jest
+        .spyOn(MailProvider.prototype, "sendMail")
+        .mockImplementation(async () => {});
 
-    const users = await usersRepository.findAll();
-    const isPasswordHashed = usersHelper.comparePassword(
-      user.password,
-      users[0].password
-    );
+      const user = usersFactory.generate();
+      const { status, body } = await request(user);
 
-    expect(MailProvider.prototype.sendMail).toHaveBeenCalledTimes(1);
-    expect(users).toHaveLength(1);
-    expect(users[0]).toStrictEqual(
-      expect.objectContaining({
-        id: expect.any(Number),
-        name: user.name,
-        email: user.email,
-        password: expect.any(String),
-      })
-    );
-    expect(isPasswordHashed).toBe(true);
+      expect(status).toBe(201);
+      expect(body).toStrictEqual(
+        expect.objectContaining({
+          message: "User created",
+          status: "OK",
+          statusCode: 201,
+          data: {},
+        })
+      );
+
+      const users = await usersRepository.findAll();
+      const isPasswordHashed = usersHelper.comparePassword(
+        user.password,
+        users[0].password
+      );
+
+      expect(MailProvider.prototype.sendMail).toHaveBeenCalledTimes(1);
+      expect(users).toHaveLength(1);
+      expect(users[0]).toStrictEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: user.name,
+          email: user.email,
+          password: expect.any(String),
+        })
+      );
+      expect(isPasswordHashed).toBe(true);
+    });
   });
 });
--- src/useCases/GetUsers/__tests__/integration/GetUsersController.spec.ts
+++ src/useCases/GetUsers/__tests__/integration/GetUsersController.spec.ts
@@ -1,43 +1,47 @@
-// @ts-ignore
 import supertest from "supertest";
-import { Server } from "../../../../application/api/Server";
-import { database } from "../../../../application/config/Database";
-import { usersFactory } from "../../../../tests/factories/Users";
+import {Server} from "../../../../application/api/Server";
+import {database} from "../../../../application/config/Database";
+import {usersFactory} from "../../../../tests/factories/Users";
 
-describe("GetUsersController", () => {
-  const server = new Server();
-  const app = server.app;
+const server = new Server();
+const app = server.app;
 
-  beforeEach(async () => await database.truncateTable("users"));
-  afterAll(async () => {
+beforeEach(async () => await database.truncateTable("users"));
+
+afterAll(async () => {
     await database.truncateTable("users");
     await database.disconnect();
-  });
+});
 
-  it("should list all users", async () => {
-    const users = usersFactory.generateMany(3);
-    await usersFactory.save(users);
+describe("GET /users", () => {
+    const request = async () => await supertest(app).get("/users").send();
 
-    const { status, body } = await supertest(app).get("/api/users").send();
+    describe("success", () => {
+        it("should list all users", async () => {
+            const users = usersFactory.generateMany(3);
+            await usersFactory.save(users);
 
-    expect(status).toBe(200);
-    expect(body).toStrictEqual(
-      expect.objectContaining({
-        message: "Users retrieved",
-        status: "OK",
-        statusCode: 200,
-        data: expect.any(Array),
-      })
-    );
+            const {status, body} = await request();
 
-    expect(body.data).toHaveLength(3);
-    expect(body.data[0]).toStrictEqual(
-      expect.objectContaining({
-        id: expect.any(Number),
-        name: users[0].name,
-        email: users[0].email,
-        password: expect.any(String),
-      })
-    );
-  });
+            expect(status).toBe(200);
+            expect(body).toStrictEqual(
+                expect.objectContaining({
+                    message: "Users retrieved",
+                    status: "OK",
+                    statusCode: 200,
+                    data: expect.any(Array),
+                })
+            );
+
+            expect(body.data).toHaveLength(3);
+            expect(body.data[0]).toStrictEqual(
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    name: users[0].name,
+                    email: users[0].email,
+                    password: expect.any(String),
+                })
+            );
+        });
+    });
 });
